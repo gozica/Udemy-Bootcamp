a378c12cb3b4a481c8ff81e2e5879d5a16dcaa99
gozica <109421350+gozica@users.noreply.github.com>
2024-04-20 19:32:00 -0400

    coding exercise 19

--- script.js
+++ script.js
@@ -2,7 +2,15 @@
 const message = "    TASTE THE RAINBOW!  "; //DON'T CHANGE THIS LINE PLEASE!
 
 // YOUR CODE BELOW THIS LINE:
-
 const whisper = message.toLocaleLowerCase().trim()
 
-console.log(whisper)
+//console.log(whisper)
+
+
+// DON'T TOUCH THIS LINE! (please)
+const word = "skateboard"; //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
+
+const facialHair = word.slice(5).replace('o', 'e');
+console.log(facialHair)
