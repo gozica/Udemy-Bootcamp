a6876413d1085ed5c3be43ffd12dc86fa0458002
gozica <109421350+gozica@users.noreply.github.com>
2024-10-29 16:01:20 -0400

    if statements

---
+++ js-decision-making/app.js
@@ -0,0 +1,21 @@
+//CONDITIONALS
+let random = Math.random();
+if(random < 0.5){
+    console.log("your number is less then 0.5");
+    console.log(random);
+    
+}
+
+if(random >= 0.5){
+    console.log("your number is greater then 0.5");
+}
+
+
+//CHALLENGE 
+/*If num is an even number, print out "even".
+  Don't do anything if num is an odd number.*/
+function isEven(num){
+    if(num % 2 === 0){
+        console.log(even);
+    }
+}
