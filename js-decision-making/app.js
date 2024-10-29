9e84068786b8bde25ba543ffc2d94348e6b54de5
gozica <109421350+gozica@users.noreply.github.com>
2024-10-29 16:01:40 -0400

    Update app.js

--- js-decision-making/app.js
+++ js-decision-making/app.js
@@ -1,21 +1,19 @@
 //CONDITIONALS
 let random = Math.random();
-if(random < 0.5){
-    console.log("your number is less then 0.5");
-    console.log(random);
-    
+if (random < 0.5) {
+  console.log("your number is less then 0.5");
+  console.log(random);
 }
 
-if(random >= 0.5){
-    console.log("your number is greater then 0.5");
+if (random >= 0.5) {
+  console.log("your number is greater then 0.5");
 }
 
-
-//CHALLENGE 
+//CHALLENGE
 /*If num is an even number, print out "even".
   Don't do anything if num is an odd number.*/
-function isEven(num){
-    if(num % 2 === 0){
-        console.log(even);
-    }
+function isEven(num) {
+  if (num % 2 === 0) {
+    console.log(even);
+  }
 }
