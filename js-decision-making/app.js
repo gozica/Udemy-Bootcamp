8c9641b09d7c8509c7a01e49c38ec56aeeda27ab
gozica <109421350+gozica@users.noreply.github.com>
2024-11-01 16:50:14 -0400

    else if section

--- js-decision-making/app.js
+++ js-decision-making/app.js
@@ -20,3 +20,32 @@ function isEven(num) {
 
 
 //ELSE IF
+
+const dayOfWeek = 'Wednesday';
+
+if(dayOfWeek === 'Monday'){
+console.log('I hate mondays');
+}else if (dayOfWeek === 'Tuesday'){
+  console.log('yay I love tuesday');
+} else if (dayOfWeek === 'Wednesday'){
+    console.log('its hump day');
+    
+  }
+  
+const age = 67;
+
+if (age < 5){
+  console.log('you are child. You get in free');
+} else if (age < 10){
+  console.log('You are a child. You pay $10');
+} else if (age < 65){
+  console.log('You are an adult you pay $20');
+}else if (age > 65){
+  console.log('You are a senior you pay $10');
+  
+
+}
+  
+
+
+
