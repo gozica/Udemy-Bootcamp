e66d6b92e721359bcb9106b0c14351926678c362
gozica <109421350+gozica@users.noreply.github.com>
2024-11-05 18:39:16 -0500

    array basics

---
+++ js-arrays/app.js
@@ -0,0 +1,14 @@
+//ARRAYS
+
+const lottoNumbers = [23, 24, 25, 2, 2, 1] //basic array of numbers
+
+const leaderboard = ["Harry", "Luna", "Hermione", "Bellatrix"];
+
+leaderboard[3] = "Draco" //changed Luna to "Draco"
+
+const planets = ["The Moon", "Venus", "Earth", "Mars", "Jupiter"];
+
+planets.shift("The Moon") // removes element from the beginning
+planets.push("Saturn") // adds the element to the end
+planets.unshift("Mercury") // adds the element to the beginning
+
